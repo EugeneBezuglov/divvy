eb5e3e88a0517f4c8bb5608d87b8dca32a7a1927
EugeneBezuglov <135106865+EugeneBezuglov@users.noreply.github.com>
2023-05-31 11:22:48 +0300

    Delete Count_DISTINCT_Station_Name_Id.sql

--- Count_DISTINCT_Station_Name_Id.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-	COUNT(DISTINCT start_station_id) AS start_station_id,
-	COUNT(DISTINCT end_station_id) AS end_station_id,
-	COUNT(DISTINCT start_station_name) AS start_station_name,
-	COUNT(DISTINCT end_station_name) AS end_station_name
-FROM full_tripdata;
