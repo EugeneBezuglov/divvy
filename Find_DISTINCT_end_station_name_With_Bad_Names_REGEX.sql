27a42940577ce371d240711276e6d1b03e981c93
EugeneBezuglov <135106865+EugeneBezuglov@users.noreply.github.com>
2023-05-31 11:24:17 +0300

    Delete Find_DISTINCT_end_station_name_With_Bad_Names_REGEX.sql

--- Find_DISTINCT_end_station_name_With_Bad_Names_REGEX.sql
+++
@@ -1,3 +0,0 @@
-SELECT DISTINCT end_station_name
-FROM full_tripdata
-WHERE end_station_name ~* '[*]|^Pub|^City|amp;|\S(May)| - Charging|Velasquez Institute Vaccination Site|Kennedy-King Vaccination Site|Truman College Vaccination Site'
